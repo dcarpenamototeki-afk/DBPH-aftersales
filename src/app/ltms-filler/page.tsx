0506a8d2fced70702219d524a4894f3f87374ddb
dcarpenamototeki-afk <dcarpena.mototeki@gmail.com>
2026-06-13 10:31:48 +0800

    Add files via upload

---
+++ src/app/ltms-filler/page.tsx
@@ -0,0 +1,10 @@
+import { AppShell } from "@/components/shell";
+import { LtmsFillerPage } from "@/components/ltms-filler-page";
+
+export default function Page() {
+  return (
+    <AppShell>
+      <LtmsFillerPage />
+    </AppShell>
+  );
+}
