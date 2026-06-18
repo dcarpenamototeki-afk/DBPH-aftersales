23d7435d53bf517f5fe7f48157321f428588ed31
dcarpenamototeki-afk <dcarpena.mototeki@gmail.com>
2026-06-18 10:00:10 +0800

    Add files via upload

---
+++ src/app/mc-release/page.tsx
@@ -0,0 +1,10 @@
+import { AppShell } from "@/components/shell";
+import { McReleasePage } from "@/components/mc-release-page";
+
+export default function Page() {
+  return (
+    <AppShell>
+      <McReleasePage />
+    </AppShell>
+  );
+}
